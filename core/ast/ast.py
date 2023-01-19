2e328768bb4eb0a1513f27008f2544f4a6a9c3f5
Antony Redman <RumataYounger@gmail.com>
2023-01-19 17:08:34 +0300

    new(core) Add AST

--- core/ast/ast.py
+++ core/ast/ast.py
@@ -1,14 +1,82 @@
-from typing import Self
+from typing import Self, List, Optional
+from dataclasses import dataclass
 
 
+@dataclass()
 class ASTNode:
+    """An AST node containing information about specific part of a query
+    Can contian other nodes inside it for nested structure
+    """
+    name: str
+    children: Optional[List[str] | List[Self]] = None
 
-    def __init__(self, name: str, content: str | Self):
-        self.name = name
-        self.content =  content
+    def __iter__(self):
+        return self.children
+
+    def to_query(self):
+        children = self.children
+        if isinstance(self.children, ASTNode):
+            children = [obj.to_query() for obj in self.children]
+        return {
+            self.name: children
+        }
+
+    def get_child(self, name: str) -> Optional[Self]:
+
+        for child in self.children:
+            if child.name == name:
+                return child
+
+            inner_search = child.get_child(name)
+            if inner_search is not None:
+                return inner_search
+
+    @classmethod
+    def get_args(cls, annotations: dict, **kwargs) -> list:
+        params = []
+
+        for k, v in kwargs.items():
+            field_val = annotations.get(k, False)
+
+            if not field_val:
+                continue
+
+            if not isinstance(v, field_val):
+                raise Exception(
+                    f"Wrong attribute type: is {type(v)}, must be {field_val}"
+                )
+
+            if isinstance(v, dict):
+                node = cls.from_args(**v)
+                params.append(node)
+                continue
+
+            params.append(f"{k} = {v}")
+
+        return params
+
+    @classmethod
+    def from_args(cls, members: list, node_name: str, **kwargs) -> Self:
+
+        param_lists = [cls.get_args(member.__annotations__, **kwargs) for member in members]
+        node = cls(name=node_name, children=param_lists)
+
+        return node
 
 
+@dataclass()
 class AST:
+    """An Abstract Syntax Tree (AST) of the resulting SQL query
+    Forms a tree structure containing multiple nodes with related information.
+    Can be extended or modified for purposes of creating full AST of any given
+    query
+    """
+    root: ASTNode = ASTNode(name="root")
 
-    def __getattr__(self, item):
+    def __getitem__(self, item):
+        for child in self.root:
+            child.get_child(item)
+
+    def __setitem__(self, key, value):
+        pass
 
