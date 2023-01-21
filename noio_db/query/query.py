b45b5562475dbcd4fd4d470eae22c0b37e77e455
Antony Redman <RumataYounger@gmail.com>
2023-01-21 05:37:27 +0300

    fix(pipeline) make version compatible with 3.9

--- noio_db/query/query.py
+++ noio_db/query/query.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 from models import BaseNoIoObject
 
@@ -7,7 +7,7 @@ class Query:
     def __init__(self):
 
         self.objects: List[BaseNoIoObject] = []
-        self.query_ast: Dict[str, str] | None = None
+        self.query_ast: Union[Dict[str, str], None] = None
 
     def __getitem__(self, item):
         pass
